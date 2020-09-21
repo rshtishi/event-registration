f2f481d71bba2cd000204fa0ab607788f9399f6b
rshtishi <rando.shtishi@gmail.com>
2020-09-21 14:23:23 +0200

    creating the database tables for event-registration-app

---
+++ event-registration-app/src/main/java/com/github/rshtishi/app/controller/HomeController.java
@@ -0,0 +1,5 @@
+package com.github.rshtishi.app.controller;
+
+public class HomeController {
+
+}
---
+++ event-registration-app/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO event VALUES (1, "JAVA SUMMIT 2020", "Tirana",  "2020-09-17 18:00:00.000", "2020-09-17 20:00:00.000");
+
+INSERT INTO attendee VALUES (1, "Rando Shtishi", "rshtishi@gmail.com","1");
+INSERT INTO attendee VALUES (2, "Arben Rami", "arami@gmail.com","1");
+INSERT INTO attendee VALUE  (3, "Ernest Wild", "ewild@gmail.com","1");
