6d77c85b5280e97bbf462674e91235a7f010963d
rshtishi <rando.shtishi@gmail.com>
2020-09-21 13:53:40 +0200

    initial commit

---
+++ email-service/src/test/java/com/github/rshtishi/emailservice/EmailServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package com.github.rshtishi.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ event-registration-app/src/test/java/com/github/rshtishi/app/EventRegistrationApplicationTest.java
@@ -0,0 +1,14 @@
+package com.github.rshtishi.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class EventRegistrationApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
