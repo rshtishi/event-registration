f4a09565babcf082a3a82b5c753b6a612e9fb3f1
rshtishi <rando.shtishi@gmail.com>
2020-09-22 16:32:29 +0200

    implementing event controller

--- event-registration-app/src/main/java/com/github/rshtishi/app/configuration/WebConfiguration.java
+++ event-registration-app/src/main/java/com/github/rshtishi/app/configuration/WebConfiguration.java
@@ -18,5 +18,6 @@ public class WebConfiguration implements WebMvcConfigurer {
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("home");
 		registry.addViewController("/login");
+		registry.addViewController("/events");
 	}
 }
--- event-registration-app/src/main/java/com/github/rshtishi/app/controller/EventController.java
+++ event-registration-app/src/main/java/com/github/rshtishi/app/controller/EventController.java
@@ -1,16 +1,47 @@
 package com.github.rshtishi.app.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.github.rshtishi.app.dto.EventDto;
+import com.github.rshtishi.app.service.IEventService;
 
 @Controller
 @RequestMapping("/events")
 public class EventController {
 	
+	@Autowired
+	private IEventService eventService;
+
 	@GetMapping("/new")
-	public String showNewRecipeForm() {
+	public String showNewEventForm(Model model) {
+		model.addAttribute("eventDto", new EventDto());
 		return "new-event";
+	}
+
+	@PostMapping("/new")
+	public String addEvent(@Valid EventDto eventDto, Errors errors, Model model) {
+		if (errors.hasErrors()) {
+			return "new-event";
+		}
+		return "redirect:/events";
+	}
+
+	@GetMapping
+	public String showEvents(Model model) {
+		List<EventDto> events = eventService.findAllEvents();
+		System.out.println(events);
+		model.addAttribute("events",events);
+		return "events";
 	}
 
 }
