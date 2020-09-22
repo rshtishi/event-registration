926d2319f527c9a66046f8ab0a0d5d452a507e79
rshtishi <rando.shtishi@gmail.com>
2020-09-22 13:17:48 +0200

    implementing security

---
+++ event-registration-app/src/main/java/com/github/rshtishi/app/configuration/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.github.rshtishi.app.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		String password = passwordEncoder().encode("password");
+		auth.inMemoryAuthentication().withUser("user").password(password).authorities("ROLE_USER");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/events/new").access("hasRole('ROLE_USER')").antMatchers("/", "**")
+				.access("permitAll").and().formLogin().loginPage("/login").defaultSuccessUrl("/events/new").and()
+				.logout().logoutSuccessUrl("/");
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
--- event-registration-app/src/main/java/com/github/rshtishi/app/configuration/WebConfiguration.java
+++ event-registration-app/src/main/java/com/github/rshtishi/app/configuration/WebConfiguration.java
@@ -1,15 +1,22 @@
 package com.github.rshtishi.app.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfiguration implements WebMvcConfigurer {
-	
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/images/**", "/css/**", "/js/**").addResourceLocations(
-				"classpath:/static/images/", "classpath:/static/css/",
-				"classpath:/static/js/");
+		registry.addResourceHandler("/images/**", "/css/**", "/js/**").addResourceLocations("classpath:/static/images/",
+				"classpath:/static/css/", "classpath:/static/js/");
 	}
 
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/login");
+	}
 }
