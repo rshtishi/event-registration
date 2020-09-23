42fa6b7094b61d5aff07072ca29889376f2294e1
rshtishi <rando.shtishi@gmail.com>
2020-09-23 15:21:14 +0200

    minor fixes

--- event-registration-app/src/main/java/com/github/rshtishi/app/controller/EventController.java
+++ event-registration-app/src/main/java/com/github/rshtishi/app/controller/EventController.java
@@ -2,14 +2,9 @@ package com.github.rshtishi.app.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
