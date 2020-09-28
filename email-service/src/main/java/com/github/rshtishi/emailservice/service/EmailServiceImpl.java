28ff352ffa9a57ae513d3b42fb40ffbeff4c9df8
rshtishi <rando.shtishi@gmail.com>
2020-09-28 11:08:26 -0700

    implementing a simple send email

--- email-service/src/main/java/com/github/rshtishi/emailservice/service/EmailServiceImpl.java
+++ email-service/src/main/java/com/github/rshtishi/emailservice/service/EmailServiceImpl.java
@@ -1,6 +1,7 @@
 package com.github.rshtishi.emailservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +16,12 @@ public class EmailServiceImpl implements EmailService {
 	
 	@Override
 	public void sendEmail(Mail mail) {
-		// TODO Auto-generated method stub
-
+	       SimpleMailMessage message = new SimpleMailMessage(); 
+	        message.setFrom(mail.getFrom());
+	        message.setTo(mail.getTo()); 
+	        message.setSubject(mail.getSubject()); 
+	        message.setText(mail.getContent());
+	        emailSender.send(message);
 	}
 
 }
---
+++ email-service/src/test/java/com/github/rshtishi/emailservice/service/EmailServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.github.rshtishi.emailservice.service;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.github.rshtishi.emailservice.entity.Mail;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class EmailServiceImplTest {
+	
+	@Autowired
+	private EmailService emailService;
+
+	@Test
+	@Order(1)
+	void testSendMail() {
+		//setup
+		Mail mail = new Mail("mr.shtishi@mail.com", "rando.shtishi@gmail.com", "hope", "this worked");
+		//execute
+		emailService.sendEmail(mail);
+		//verify
+		
+	}
+
+}
