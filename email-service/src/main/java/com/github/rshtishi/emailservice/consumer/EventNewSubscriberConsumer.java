b655682669568cd848fe6e893fe38f9213973380
rshtishi <rando.shtishi@gmail.com>
2020-10-02 14:47:55 +0200

    implementing the mail delivery on receiving a new registraion

--- email-service/src/main/java/com/github/rshtishi/emailservice/consumer/EventNewSubscriberConsumer.java
+++ email-service/src/main/java/com/github/rshtishi/emailservice/consumer/EventNewSubscriberConsumer.java
@@ -1,16 +1,21 @@
 package com.github.rshtishi.emailservice.consumer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 import com.github.rshtishi.emailservice.entity.EventSubscriber;
+import com.github.rshtishi.emailservice.service.EventNotificationService;
 
 @Component
 public class EventNewSubscriberConsumer {
 	
+	@Autowired
+	private EventNotificationService eventNotificationService;
+	
 	@JmsListener(destination = "new.registration")
-	public void consumeMessage(EventSubscriber message) {
-		System.out.println(">>>>>> :"+message);
+	public void consumeMessage(EventSubscriber subscriber) {
+		eventNotificationService.notifyEventSubscriberByEmail(subscriber);
 	}
 
 }
