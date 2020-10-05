d962091a6b26bd6d403457da13b6f6399cbb3bc5
rshtishi <rando.shtishi@gmail.com>
2020-10-05 16:46:35 +0200

    configuring rabbitmq

--- event-registration-app/src/main/java/com/github/rshtishi/app/configuration/RabbitMQConfiguration.java
+++ event-registration-app/src/main/java/com/github/rshtishi/app/configuration/RabbitMQConfiguration.java
@@ -1,11 +1,42 @@
 package com.github.rshtishi.app.configuration;
 
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-
 
 @Configuration
 public class RabbitMQConfiguration {
-	
 
+	@Value("${spring.rabbitmq.host}")
+	private String host;
+	@Value("${spring.rabbitmq.username}")
+	private String user;
+	@Value("${spring.rabbitmq.password}")
+	private String password;
+	@Value("${spring.rabbitmq.port}")
+	private Integer port;
+	@Value("${rabbitmq.routing-key}")
+	private String routingKey ;
+
+	@Bean
+	public ConnectionFactory connectionFactory() {
+		CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host);
+		connectionFactory.setUsername(user);
+		connectionFactory.setPassword(password);
+		connectionFactory.setPort(port);
+		return connectionFactory;
+	}
+
+	@Bean
+	public RabbitTemplate rabbitTemplate() {
+		RabbitTemplate rabbitTemplate = new RabbitTemplate();
+		rabbitTemplate.setConnectionFactory(connectionFactory());
+		rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+		rabbitTemplate.setRoutingKey(routingKey);
+		return rabbitTemplate;
+	}
 }
--- event-registration-app/src/main/java/com/github/rshtishi/app/producer/RegistrationEventProducer.java
+++ event-registration-app/src/main/java/com/github/rshtishi/app/producer/RegistrationEventProducer.java
@@ -1,10 +1,22 @@
 package com.github.rshtishi.app.producer;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import com.github.rshtishi.app.dto.AttendeeDto;
 
 @Component
 public class RegistrationEventProducer {
 
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+	@Value("${rabbitmq.routing-key}")
+	private String destination ;
 	
+    public void sendTo(AttendeeDto attendee) {
+        rabbitTemplate.convertAndSend(destination, attendee);
+    }
 
 }
